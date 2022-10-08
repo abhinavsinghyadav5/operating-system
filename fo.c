16eb2cd79b66d7192f344333f299ec72657c0f07
abhinavsinghyadav5 <91362425+abhinavsinghyadav5@users.noreply.github.com>
2022-10-08 22:30:53 +0530

    Update fo.c

--- fo.c
+++ fo.c
@@ -2,7 +2,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 int main()
-{   printf("Abhinav Singh Yadav StudentId-20011780\n");
+{   printf("Abhinav Singh Yadav \n");
     fork();
     printf("Hello Abhinav the Process id is:- %d\n",getpid());
 	return 0;
